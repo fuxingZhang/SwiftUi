025c61cb3391abe45280ed65716953eb5020a603
张复星 <star@zhangfuxingdeMacBook-Pro.local>
2020-01-18 12:25:36 +0800

    first commit

---
+++ example/star/page/Image/WebImagePage.swift
@@ -0,0 +1,21 @@
+//
+//  WebImagePage.swift
+//  star
+//
+//  Created by 张复星 on 2020/1/18.
+//  Copyright © 2020 张复星. All rights reserved.
+//
+
+import SwiftUI
+
+struct WebImagePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WebImagePage_Previews: PreviewProvider {
+    static var previews: some View {
+        WebImagePage()
+    }
+}
